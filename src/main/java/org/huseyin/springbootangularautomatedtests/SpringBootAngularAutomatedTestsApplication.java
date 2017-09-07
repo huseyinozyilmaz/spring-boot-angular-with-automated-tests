008ca6aca34e3c98ff1abb69961567eb43d675e6
Huseyin M Ozyilmaz <huseyinozyilmaz@users.noreply.github.com>
2017-09-07 11:22:41 +0100

    Initial source code of the spring boot application

---
+++ src/main/java/org/huseyin/springbootangularautomatedtests/SpringBootAngularAutomatedTestsApplication.java
@@ -0,0 +1,12 @@
+package org.huseyin.springbootangularautomatedtests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularAutomatedTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularAutomatedTestsApplication.class, args);
+	}
+}
